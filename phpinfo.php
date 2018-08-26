53968e2326ef04064e27a7eeada19fd380101c26
xuxin3101 <xuxin3101@gmail.com>
2018-08-26 23:02:18 +0800

    s

--- phpinfo.php
+++ phpinfo.php
@@ -1,3 +1,18 @@
 <?php
 phpinfo();
+include('connect.php');
+header("Access-Control-Allow-Origin: *");
+    $data=[];
+    $sql = "select title,logo,id from jiaoben";
+    $res = $mysqli->query($sql);
+    if (!$res) {
+        die("sql error:\n" . $mysqli->error);
+    }
+    while ($row= $res->fetch_assoc()) {
+        array_push($data,$row);
+    }
+    echo json_encode($data);
+
+
+$mysqli->close();
 ?>
