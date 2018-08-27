92dae00186ffdec773943b1ad43048f2647cec05
xuxin3101 <xuxin3101@gmail.com>
2018-08-27 09:47:59 +0800

    s

--- api/getpage.php
+++ api/getpage.php
@@ -1,7 +1,7 @@
 <?php
 include('connect.php');
 header("Access-Control-Allow-Origin: *");
-    $sql = "select count(*) shuliang from jiaoben";
+$sql = "select count(*) shuliang from jiaoben";
     $res = $mysqli->query($sql);
     if (!$res) {
         die("sql error:\n" . $mysqli->error);
@@ -9,5 +9,4 @@ header("Access-Control-Allow-Origin: *");
 $row= $res->fetch_assoc();
 echo json_encode($row);
 $mysqli->close();
-
 ?>
