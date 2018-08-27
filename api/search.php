820a74dabb76e80016abe536414cea35b48e6086
xuxin3101 <xuxin3101@gmail.com>
2018-08-27 19:33:54 +0800

    s

--- api/search.php
+++ api/search.php
@@ -5,6 +5,7 @@ if (!empty($_POST['keyword'])) {
     $data=[];
     $keyword=$_POST['keyword'];
     $sql = "select title,logo,id from jiaoben where content like '%$$keyword%';";
+    echo $sql;
     $res = $mysqli->query($sql);
     if (!$res) {
         die("sql error:\n" . $mysqli->error);
