246c78541106dc22895bafbe99fe31c9669ba4cf
xuxin3101 <xuxin3101@gmail.com>
2018-08-27 20:02:56 +0800

    s

--- api/getdetail.php
+++ api/getdetail.php
@@ -9,6 +9,10 @@ if (!$res) {
     die("sql error:\n" . $mysqli->error);
 }
 $row= $res->fetch_assoc();
+$cishu=$row['cishu'];
+$cishu=$cishu+1;
+$sql="update jiaoben set cishu=$cishu where id = $id";
+$res = $mysqli->query($sql);
 echo json_encode($row);
 }
 else
