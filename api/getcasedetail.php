43f3b8a50fec40c70e9c44aa634aa77f18a07d81
xuxin3101 <xuxin3101@gmail.com>
2018-08-29 07:21:41 +0800

    s

--- api/getcasedetail.php
+++ api/getcasedetail.php
@@ -3,7 +3,7 @@ include('connect.php');
 header("Access-Control-Allow-Origin: *");
 if (!empty($_POST['id'])) {
 $id=$_POST['id'];
-$sql="select * from case where id=$id";
+$sql="select * from case1 where id=$id";
 $res = $mysqli->query($sql);
 if (!$res) {
     die("sql error:\n" . $mysqli->error);
@@ -11,7 +11,7 @@ if (!$res) {
 $row= $res->fetch_assoc();
 $cishu=$row['count'];
 $cishu=$cishu+1;
-$sql="update case set count=$cishu where id = $id";
+$sql="update case1 set count=$cishu where id = $id";
 $res = $mysqli->query($sql);
 echo json_encode($row);
 }
--- api/getdecasepage.php
+++ api/getdecasepage.php
@@ -2,7 +2,7 @@
 include('connect.php');
 header("Access-Control-Allow-Origin: *");
 
-    $sql = "select count(*) shuliang from case";
+    $sql = "select count(*) shuliang from case1";
     $res = $mysqli->query($sql);
     if (!$res) {
         die("sql error:\n" . $mysqli->error);
