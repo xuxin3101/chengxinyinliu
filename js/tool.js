0d3d2f722ab191ef131b426b50cec03c8a817402
xuxin3101 <xuxin3101@gmail.com>
2018-08-29 07:18:54 +0800

    s

--- js/tool.js
+++ js/tool.js
@@ -275,7 +275,80 @@ function searchdemo(){
 	var shanng=document.getElementById("shang")
 	var xia=document.getElementById("xia")
 	http.post({
-		url: "./api/getdemodetail.php",
+		url: "./api/getcasedetail.php",
+		data: "id=" + id,
+		timeout: 1000
+	}, function (err, result) {
+		var data = result
+		if (typeof data == 'string')
+			data = JSON.parse(data.trim());
+		var time = document.getElementById("time");
+		var cishu = document.getElementById("cishu");
+		var title = document.getElementById("title");
+		var container = document.getElementById("container");
+		document.title = data.title
+		cishu.innerHTML = data.count
+		title.innerHTML = data.title
+		time.innerHTML = data.time
+		var content=data.content
+		content=content.split(";")
+		for(var idx in content){
+			var item=content[idx];
+			item=item.split(":")
+			var tmp=document.createElement(item[0])
+			tmp.src=item[1]
+			if(item[0]=='video')
+			{tmp.height="500"
+			tmp.width="500"}
+			container.appendChild(tmp)
+		}
+	})
+	var tmpid=id+1
+		http.post({
+		url: "./api/getcasedetail.php",
+		data: "id=" + tmpid,
+		timeout: 1000
+	}, function (err, result) {
+		var data = result
+		if (typeof data == 'string')
+			data = JSON.parse(data.trim());
+		if(data==null){
+				xia.innerHTML="下一篇：没有了"
+		}else{
+			xia.innerHTML="下一篇："+data.title
+		}
+		
+	})
+	var shangid=id-1
+	if(shangid==0)
+		shang.innerHTML="上一篇：没有了"
+	else{
+		http.post({
+		url: "./api/getcasedetail.php",
+		data: "id=" + shangid,
+		timeout: 1000
+	}, function (err, result) {
+		var data = result
+		if (typeof data == 'string')
+			data = JSON.parse(data.trim());
+		if(data==null){
+				shang.innerHTML="上一篇：没有了"
+		}else{
+			shang.innerHTML="上一篇："+data.title
+		}
+		
+	})
+	}
+
+	
+
+}
+function searchcase(){
+	var id = getpara().id
+	var shanng=document.getElementById("shang")
+	var xia=document.getElementById("xia")
+	http.post({
+		url: "./api/getcasedetail.php",
 		data: "id=" + id,
 		timeout: 1000
 	}, function (err, result) {
@@ -295,7 +368,7 @@ function searchdemo(){
 	})
 	var tmpid=id+1
 		http.post({
-		url: "./api/getdemodetail.php",
+		url: "./api/getcasedetail.php",
 		data: "id=" + tmpid,
 		timeout: 1000
 	}, function (err, result) {
@@ -314,7 +387,7 @@ function searchdemo(){
 		shang.innerHTML="上一篇：没有了"
 	else{
 		http.post({
-		url: "./api/getdemodetail.php",
+		url: "./api/getcasedetail.php",
 		data: "id=" + shangid,
 		timeout: 1000
 	}, function (err, result) {
