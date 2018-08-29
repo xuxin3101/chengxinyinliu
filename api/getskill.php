54b5a2583081d5f611021eefb4e9da65c80f914b
xuxin3101 <xuxin3101@gmail.com>
2018-08-29 10:30:02 +0800

    s

---
+++ api/getskill.php
@@ -0,0 +1,33 @@
+<?php
+include('connect.php');
+header("Access-Control-Allow-Origin: *");
+if (empty($_GET['page'])) {
+    $data=[];
+    $sql = "select title,logo,id,time,count from skill order by id  limit 0,9;";
+    $res = $mysqli->query($sql);
+    if (!$res) {
+        die("sql error:\n" . $mysqli->error);
+    }
+    while ($row= $res->fetch_assoc()) {
+        array_push($data, $row);
+    }
+    echo json_encode($data);
+}
+else{
+$page=$_GET['page'];
+$start=($page-1)*10;
+$end=$start+9;
+$data=[];
+$sql = "select title,logo,id,time,count from skill order by id  limit $start,$end;";
+$res = $mysqli->query($sql);
+if (!$res) {
+    die("sql error:\n" . $mysqli->error);
+}
+while ($row= $res->fetch_assoc()) {
+    array_push($data, $row);
+}
+echo json_encode($data);
+}
+$mysqli->close();
+?>
+
