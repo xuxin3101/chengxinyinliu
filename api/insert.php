af5abb8394b37bceef0c1c71240e8bb2eaeae3d3
xuxin3101 <xuxin3101@gmail.com>
2018-08-29 21:21:01 +0800

    s

---
+++ api/insert.php
@@ -0,0 +1,19 @@
+<?php
+include('connect.php');
+header("Access-Control-Allow-Origin: *");
+if (empty($_POST['name'])) {
+    $name=$_POST['name'];
+    $phone=$_POST['phone'];
+    $mail=$_POST['mail'];
+    $content=$_POST['content'];
+    $sql = "insert into msg(id,name,phone,mail,content) values(null,'$name','$phone','$mail','$content')";
+    $res = $mysqli->query($sql);
+    if (!$res) {
+        die("sql error:\n" . $mysqli->error);
+    }
+    echo $res;
+}
+
+$mysqli->close();
+?>
+
