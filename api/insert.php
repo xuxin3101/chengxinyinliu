ab6c1fc6b64b26268e780ceca4dec14422b83474
xuxin3101 <xuxin3101@gmail.com>
2018-08-29 21:30:29 +0800

    s

--- api/insert.php
+++ api/insert.php
@@ -1,18 +1,21 @@
 <?php
 include('connect.php');
 header("Access-Control-Allow-Origin: *");
-if (empty($_POST['name'])) {
+if (!empty($_POST['name'])) {
     $name=$_POST['name'];
     $phone=$_POST['phone'];
     $mail=$_POST['mail'];
     $content=$_POST['content'];
     $sql = "insert into msg(id,name,phone,mail,content) values(null,'$name','$phone','$mail','$content')";
-    echo $sql;
     $res = $mysqli->query($sql);
     if (!$res) {
         die("sql error:\n" . $mysqli->error);
     }
-    echo $sql;
+    echo $res;
+}
+else
+{
+    echo '1';
 }
 
 $mysqli->close();
